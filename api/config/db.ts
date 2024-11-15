883665f80f14a2c53c64c13a2e9da344a7296464
Storm Llamas <stormsiallamas@gmail.com>
2024-11-15 19:09:10 +0800

    Data seeding

--- api/config/db.ts
+++ api/config/db.ts
@@ -1,28 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
 
 const MONGO_DB_URL = process.env.MONGO_DB_URL || "";
-const client = new MongoClient(MONGO_DB_URL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
 
 export const connectDB = async () => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    const conn = await mongoose.connect(MONGO_DB_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
