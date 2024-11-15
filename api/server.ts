b52270848c4f7c6011f31f9fd314fbb9973b2e53
Storm Llamas <stormsiallamas@gmail.com>
2024-11-15 16:11:59 +0800

    Setup dotenv

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products";
+
+const PORT = process.env.PORT || 8000
 
 const app = express();
 
@@ -8,8 +12,6 @@ app.get("/", (req: Request, res: Response): any => res.json(products));
 app.get("/api/products/:id", (req: Request, res: Response): any => {
   const product = products.find((p) => p._id === req.params.id);
   return res.json(product);
-});
-
-const PORT = process.env.PORT || 4000;
+});;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
