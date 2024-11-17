e4c20ddbc694625e3498e97379ab13954fed3a12
Storm Llamas <stormsiallamas@gmail.com>
2024-11-17 15:35:45 +0800

    allowImportingTsExtensions: true

--- api/controllers/products.ts
+++ api/controllers/products.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import asyncHandler from "../middleware/asyncHandler";
+import asyncHandler from "../middleware/asyncHandler.ts";
 import Product from "../models/productModel";
 
 export const getAllProducts = asyncHandler(async (_, res: Response) => {
