0a9d380bcd7c1b475498a060d630f0adfe963f2d
Storm Llamas <stormsiallamas@gmail.com>
2024-11-18 00:19:41 +0800

    Configure for production

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import path from 'path';
 dotenv.config();
 import express, { Request, Response } from "express";
 import { connectDB } from "./config/db";
@@ -12,6 +13,23 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve(); 
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // All the requests that are not for the API should be redirected to the frontend
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
